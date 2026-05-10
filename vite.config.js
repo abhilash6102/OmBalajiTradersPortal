723080cbc8838958f80769a2532c6cdeead418a7
abhilash6102 <kanchukota.abhilash6102@gmail.com>
2026-05-10 20:17:25 +0530

    changed

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { fileURLToPath, URL } from 'url';
 
+// https://vite.dev/config/
 export default defineConfig({
   base: './', 
-  logLevel: 'error', 
   plugins: [react()],
   resolve: {
     alias: {
